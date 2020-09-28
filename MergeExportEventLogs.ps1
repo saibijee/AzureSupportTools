0567407376937c214b34cb8ec05d35a1cf5e2f7a
saibijee <45789154+saibijee@users.noreply.github.com>
2020-09-28 18:30:47 +0100

    Update MergeExportEventLogs.ps1

--- MergeExportEventLogs.ps1
+++ MergeExportEventLogs.ps1
@@ -182,6 +182,6 @@ Remove-Item -Path "$directory\$usage.txt" -Force
 
 write-host "AzureSupportTools will self-update now and this PowerShell Window will self-close in 10 seconds..." -ForegroundColor Green
 
-(iwr -Uri https://raw.githubusercontent.com/saibijee/AzureSupportTools/master/install.ps1 -UseBasicParsing).content | iex
+(iwr -Uri https://raw.githubusercontent.com/saibijee/AzureSupportTools/master/install.ps1 -UseBasicParsing).content | iex | out-null
 
 Start-Sleep -Seconds 10
