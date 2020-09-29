96bc3fbe463c818e1b6777a19b80df90514a4f19
saibijee <45789154+saibijee@users.noreply.github.com>
2020-09-29 10:38:58 +0100

    Update TextFileSearcher.ps1

--- TextFileSearcher.ps1
+++ TextFileSearcher.ps1
@@ -89,7 +89,7 @@ $ProgressPreference = "SilentlyContinue"
 Set-AzureStorageFileContent -ShareName "tmusage" -Context $ctx -Source "$directory\$usage.txt" -ErrorAction Stop
 $ProgressPreference = $OriginalPref
 Remove-Item -Path "$directory\$usage.txt" -Force
-"Telemery Updated"
+"Telemetry Updated"
 
 }catch{
 "Telemetry Failed"
