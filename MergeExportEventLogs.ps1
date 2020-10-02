9e407d70c80ed1219bfd6974991fc68ec2e7ade7
Sohaib Shaheed <45789154+saibijee@users.noreply.github.com>
2020-10-02 15:33:36 +0100

    Update MergeExportEventLogs.ps1

--- MergeExportEventLogs.ps1
+++ MergeExportEventLogs.ps1
@@ -162,6 +162,7 @@ try{
 
 
 #Telemetry
+<#
 try{
 # Check Az PowerShell Modules
 
@@ -185,7 +186,7 @@ try{
     }
 }catch{
        Write-Host "Azure Powershell Version needs Installing/Updating. Please run 'Install-Module -Name Az -AllowClobber' as an admin " -ForegroundColor Yellow;
-}
+}#>
 
 try{
     $usage = Get-Date -UFormat "%Y%m%d%H%M%S%Z"
