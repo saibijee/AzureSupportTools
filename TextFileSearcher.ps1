3797b815e049dd6e48d4b17eff4cc52cf4c76755
Sohaib Shaheed <45789154+saibijee@users.noreply.github.com>
2020-10-02 11:53:17 +0100

    Update TextFileSearcher.ps1

--- TextFileSearcher.ps1
+++ TextFileSearcher.ps1
@@ -54,47 +54,51 @@ try{
 }
 
 
+#Telemetry
 try{
-
 # Check Az PowerShell Modules
 
     $AzModuleVersion = Get-InstalledModule -Name az -ErrorAction SilentlyContinue
     if (($AzModuleVersion.version.major -ge 3) -and ($AzModuleVersion.version.minor -ge 2)){
-        #Write-Host "Azure PowerShell Version is greater than 3.2.0"
+         Write-Host "Azure PowerShell Version is $($AzmoduleVersion.Version)" -ForegroundColor Green
     }else{
         if($AzModuleVersion.Version.GetType().Name -eq "Version")
             {
                 Write-Host "Azure Powershell Version needs updating. Please run 'Install-Module -Name Az -AllowClobber' as an admin ";
-                Write-Error "Azure Powershell Version older than what the script has been tested with" -ErrorAction Stop
+                
             }else{
-                "Get-InstalledModule -Name Az returned a String, comparing"
+                #"Get-InstalledModule -Name Az returned a String, comparing"
                 if(($AzModuleVersion.Version.Substring(0,$AzModuleVersion.Version.IndexOf(".")) -ge 3) -and ($AzModuleVersion.Version.Substring($AzModuleVersion.Version.IndexOf(".")+1,$AzModuleVersion.Version.lastindexof(".")-2) -ge 2)){
-                    #Write-Host "Azure PowerShell Version is greater than 3.2.0"
+                    Write-Host "Azure PowerShell Version is $($AzmoduleVersion.Version)" -ForegroundColor Green
                 }else{
                     Write-Host "Azure Powershell Version needs updating. Please run 'Install-Module -Name Az -AllowClobber' as an admin ";
-                    Write-Error "Azure Powershell Version older than what the script has been tested with" -ErrorAction Stop
                 }
 
             }
     }
-
-$usage = Get-Date -UFormat "%Y%m%d%H%M%S%Z"
-$usage|Out-File "$directory\$usage-$env:username.txt"
-$connectionstring = "FileEndpoint=https://supporttoolusage.file.core.windows.net/;SharedAccessSignature=sv=2019-12-12&ss=f&srt=o&sp=rw&se=2020-12-31T23:59:59Z&st=2020-09-28T13:12:27Z&spr=https&sig=delYLuwJblMImm2jGePVtNMr7P3OPioydCFhjC1NkP8%3D"
-$ctx = New-AzStorageContext -ConnectionString "$connectionstring"
-$OriginalPref = $ProgressPreference # Default is 'Continue'
-$ProgressPreference = "SilentlyContinue"
-Set-AzStorageFileContent -ShareName "tmusage" -Context $ctx -Source "$directory\$usage-$env:username.txt" -ErrorAction Stop -ClientTimeOutPerRequest 10 -asjob|Out-Null
-start-sleep -Seconds 5
-$ProgressPreference = $OriginalPref
-Remove-Item -Path "$directory\$usage-$env:username.txt" -Force
-"Telemetry Updated"
-
 }catch{
-"Telemetry Failed"
+       Write-Host "Azure Powershell Version needs Installing/Updating. Please run 'Install-Module -Name Az -AllowClobber' as an admin " -ForegroundColor Yellow;
 }
 
-write-host "AzureSupportTools will self-update now and this PowerShell Window will self-close in 10 seconds..." -ForegroundColor Green
+try{
+    $usage = Get-Date -UFormat "%Y%m%d%H%M%S%Z"
+    $telemetryfilename = "$ENV:LOCALAPPDATA\AzureSupportUtilities\$usage-$env:username-TEXT.txt"
+    $usage|Out-File $telemetryfilename
+    $connectionstring = "FileEndpoint=https://supporttoolusage.file.core.windows.net/;SharedAccessSignature=sv=2019-12-12&ss=f&srt=o&sp=rw&se=2020-12-31T23:59:59Z&st=2020-09-28T13:12:27Z&spr=https&sig=delYLuwJblMImm2jGePVtNMr7P3OPioydCFhjC1NkP8%3D"
+    $ctx = New-AzStorageContext -ConnectionString "$connectionstring"
+    $OriginalPref = $ProgressPreference # Default is 'Continue'
+    $ProgressPreference = "SilentlyContinue"
+    Set-AzStorageFileContent -ShareName "tmusage" -Context $ctx -Source $telemetryfilename -ErrorAction Stop -ClientTimeOutPerRequest 10 -asjob|Out-Null
+    start-sleep -Seconds 5
+    $ProgressPreference = $OriginalPref
+    Remove-Item -Path $telemetryfilename -Force
+    "Telemetry Updated"
+
+}catch{
+    "Telemetry Failed"
+}
+
+write-host "AzureSupportTools will self-update now" -ForegroundColor Green
 
 try{
 (iwr -Uri https://raw.githubusercontent.com/saibijee/AzureSupportTools/master/install.ps1 -UseBasicParsing -TimeoutSec 8 -ErrorAction SilentlyContinue).content | iex -ErrorAction SilentlyContinue | out-null
@@ -103,6 +107,6 @@ catch{
 Write-host "Update Failed" -ForegroundColor Red
 }
 
+write-host "This window will self-close in 10 seconds" -ForegroundColor Green
 Start-Sleep -Seconds 10
-
 
